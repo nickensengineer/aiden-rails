25d8d3fb852c4ab1eea01b2a03e6f386a9fc6626
nickens.engineer <nickens.engineer@gmail.com>
2019-09-29 17:31:12 -0500

    updated files, mid of part 2 guide, switched to rubymine

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_29_190255) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
